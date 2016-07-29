0435c3e8214f00f01d5e368d38879a926dde8c1b
SLIVKOKZ <ayydismemberment@gmail.com>
2016-07-29 06:04:44 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(320, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Keybind";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 46);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(76, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Stabilizator";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 69);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Auto strafe";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "63";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(222, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "12";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Autostrafe speed :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Autostrafe length :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(2, 92);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(115, 100);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "strafe direction";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 62);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(58, 17);
+            this.radioButton6.TabIndex = 3;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "sw(left)";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 39);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(64, 17);
+            this.radioButton5.TabIndex = 2;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "sw(right)";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 83);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(39, 17);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "bw";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 16);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(36, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "fw";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(222, 145);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "0";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Autostrafe vertical :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 194);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "TOP KEK";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows;
+using Microsoft.DirectX.DirectInput;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApplication6
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        int speed = 63;
+        int length = 12;
+        ushort strR = 0x20;
+        ushort strL = 0x1e;
+        
+        ushort bind1 = 0;
+        ushort bind2 = 1;
+        int autostrafeY = 0;
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct KEYBDINPUT
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct HARDWAREINPUT
+        {
+            uint uMsg;
+            ushort wParamL;
+            ushort wParamH;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct INPUT
+        {
+            [FieldOffset(0)]
+            public int type;
+            [FieldOffset(4)] //*
+            public MOUSEINPUT mi;
+            [FieldOffset(4)] //*
+            public KEYBDINPUT ki;
+            [FieldOffset(4)] //*
+            public HARDWAREINPUT hi;
+        }
+        [DllImport("user32.dll")]
+        static extern UInt32 SendInput(UInt32 nInputs, [MarshalAs(UnmanagedType.LPArray, SizeConst = 1)] INPUT[] pInputs, Int32 cbSize);
+
+        const int KEYEVENTF_DOWN = 0; //key UP
+        const int KEYEVENTF_EXTENDEDKEY = 0x0001;
+        const int KEYEVENTF_KEYUP = 0x0002; //key UP
+        const int KEYEVENTF_UNICODE = 0x0004;
+        const int KEYEVENTF_SCANCODE = 0x0008; // scancode
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        const int MOUSEEVENTF_MOVE = 0x0001;
+
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetMessageExtraInfo();
+        public static void GenerateKey(ushort vk, bool key_down)
+        {
+            INPUT[] inputs = new INPUT[1];
+            inputs[0].type = 1;
+
+            KEYBDINPUT kb = new KEYBDINPUT(); //{0};
+            TimeSpan ts = new TimeSpan(DateTime.UtcNow.Ticks - DateTime.Parse("1970-01-01T00:00:00").Ticks);
+            UInt32 time_t = Convert.ToUInt32(ts.TotalSeconds);
+            kb.wScan = vk;
+            kb.time = time_t;
+            if (key_down)
+            {
+                kb.dwFlags = (uint) ( KEYEVENTF_DOWN | KEYEVENTF_SCANCODE );
+                kb.wVk = (ushort)0;
+                kb.dwExtraInfo = GetMessageExtraInfo();
+                inputs[0].ki = kb;
+                SendInput(1, inputs, System.Runtime.InteropServices.Marshal.SizeOf(inputs[0]));
+            }
+            else
+            {
+                kb.dwFlags = (uint) ( KEYEVENTF_KEYUP | KEYEVENTF_SCANCODE );
+                kb.wVk = (ushort)0;
+                kb.dwExtraInfo = GetMessageExtraInfo();
+                inputs[0].ki = kb;
+                SendInput(1, inputs, System.Runtime.InteropServices.Marshal.SizeOf(inputs[0]));
+            }
+             
+        }
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+
+        static void move_mouse(int x, int y)
+        {
+            INPUT[] inputs = new INPUT[1];
+            inputs[0].type = 0;
+            MOUSEINPUT mi = new MOUSEINPUT();
+            mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+            mi.dx += x;
+            mi.dy += y;
+            inputs[0].mi = mi;
+            SendInput(1, inputs, System.Runtime.InteropServices.Marshal.SizeOf(inputs[0]));
+        }
+
+
+       UInt16 Butn1;
+       Thread t;
+        Device keyboard, mouse, joystick;
+        bool f = false;
+        private void InitDevices() {
+            keyboard = new Device(SystemGuid.Keyboard);
+            if (keyboard == null) throw new Exception("No keyboard found.");
+
+            //create mouse device.
+            mouse = new Device(SystemGuid.Mouse);
+            if (mouse == null)
+            {
+                throw new Exception("No mouse found.");
+            }
+            mouse.Acquire();
+        
+        }
+        public void AutoStrafe() {
+            f = true;
+            while (radioButton2.Checked)
+            {
+                
+                if (GetAsyncKeyState(Butn1) != 0) {
+                    GenerateKey(strL, true); //D
+                    for (int x = 0; x < speed; x++)
+                    {
+                        if (f)
+                        {
+                            move_mouse(-length / 2, autostrafeY);
+                        }
+                        else move_mouse(-length, autostrafeY);
+                        Thread.Sleep(1);
+                    }
+                    f = false;
+                    GenerateKey(strL, false);
+                    if (GetAsyncKeyState(Butn1) == 0) break;
+                    GenerateKey(strR, true);
+                    for (int x = 0; x < speed; x++)
+                    {
+                        move_mouse(length, -autostrafeY);
+                        Thread.Sleep(1);
+                    }
+                    GenerateKey(strR, false);
+                
+                }
+                
+            }
+            t = new Thread(new ThreadStart(StrafeHelper));
+            t.Start();
+            
+        }
+        public void StrafeHelper() {
+            while (radioButton1.Checked)
+            {
+                if (GetAsyncKeyState(Butn1) != 0)
+                {
+                    MouseState state = mouse.CurrentMouseState;
+                    if (state.X > 0)
+                    {
+                        GenerateKey(strR, true);
+                        f = true;
+                    }
+                    else if (state.X < 0)
+                    {
+                        GenerateKey(strR, false);
+                    }
+                    if (state.X < 0)
+                    {
+                        GenerateKey(strL, true);
+                        f = true;
+                    }
+                    else if (state.X > 0)
+                    {
+                        GenerateKey(strL, false);
+                    }
+                    
+                }
+                if (GetAsyncKeyState(Butn1) == 0 && f == true) {
+			        f = false;
+			        GenerateKey(strR, false);
+			        GenerateKey(strL, false);
+		        }
+                Thread.Sleep(1);
+            }
+            t = new Thread(new ThreadStart(AutoStrafe));
+            t.Start();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+            radioButton3.Checked = true;
+            InitDevices();
+            textBox1.Text = Convert.ToString(63);
+            textBox2.Text = Convert.ToString(12);
+            this.MouseDown -= Form1_MouseDown;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (t != null)
+            t.Suspend();
+            this.KeyPreview = true;
+            this.MouseDown += Form1_MouseDown;
+            button1.MouseDown += button1_MouseDown;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.Text = e.Button.ToString();
+            string x = e.Button.ToString();
+            //if (x == "Left") Butn1 = 0x01;
+            if (x == "Right") Butn1 = 0x02;
+            if (x == "Middle") Butn1 = 0x04;
+            if (x == "XButton1") Butn1 = 0x05;
+            if (x == "XButton2") Butn1 = 0x06;
+            this.MouseDown -= Form1_MouseDown;
+            button1.MouseDown -= button1_MouseDown;
+            this.KeyPreview = false;
+            if (radioButton1.Checked)
+                t = new Thread(new ThreadStart(StrafeHelper));
+            else t = new Thread(new ThreadStart(AutoStrafe));
+            t.Start();
+        }
+        private void button1_MouseDown(object sender, MouseEventArgs e) {
+            button1.Text = e.Button.ToString();
+            string x = e.Button.ToString();
+            //if (x == "Left") Butn1 = 0x01;
+            if (x == "Right") Butn1 = 0x02;
+            if (x == "Middle") Butn1 = 0x04;
+            if (x == "XButton1") Butn1 = 0x05;
+            if (x == "XButton2") Butn1 = 0x06;
+            this.MouseDown -= Form1_MouseDown;
+            button1.MouseDown -= button1_MouseDown;
+            this.KeyPreview = false;
+            if (radioButton1.Checked)
+                t = new Thread(new ThreadStart(StrafeHelper));
+            else t = new Thread(new ThreadStart(AutoStrafe));
+            t.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (t != null) t.Abort();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+                textBox1.Text = "0";
+            if (textBox2.Text == "")
+                textBox2.Text = "0";
+            speed = int.Parse(textBox1.Text);
+            length = int.Parse(textBox2.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "") {
+            length = int.Parse(textBox2.Text);
+            }
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                speed = int.Parse(textBox1.Text);
+            }
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton5.Checked)
+            {
+                strL = 0x11;
+                strR = 0x1F;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                strR = 0x20;
+                strL = 0x1e;
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                strR = 0x1e;
+                strL = 0x20;
+            }
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton6.Checked)
+            {
+                strL = 0x1F;
+                strR = 0x11;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text != "") {
+                autostrafeY = int.Parse(textBox3.Text);
+            }
+            
+        }
+
+        private void button2_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+
+        private void button3_KeyDown(object sender, KeyEventArgs e)
+        {
+                
+        }
+    }
+}
